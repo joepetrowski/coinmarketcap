c159acff9e31a91e579527356318f44ef81ea21d
joepetrowski <joe@parity.io>
2019-07-06 15:46:31 +0200

    added better error messages

--- tests.py
+++ tests.py
@@ -2,14 +2,11 @@
 from pyCMC import CMC
 
 def test_results(returnVal, tname):
-	if 'error' not in returnVal.keys():
-		if 'status' in returnVal.keys():
-			if returnVal['status']['error_code'] == 0:
-				print('{} works!'.format(tname))
-			else:
-				print('Error message: {}'.format(returnVal['status']['error_message']))
+	if 'status' in returnVal.keys():
+		if returnVal['status']['error_code'] == 0:
+			print('{} works!'.format(tname))
 		else:
-			print(returnVal)
+			print('Error message: {}'.format(returnVal['status']['error_message']))
 	else:
 		print(returnVal)
 
@@ -41,3 +38,18 @@ test_results(metrics, 'Metrics')
 # Convert Price
 convert = cmc.convert_price(2, coinId=None, symbol='ETH', convert='USD')
 test_results(convert, 'Convert')
+
+# These should return errors before calling the API
+print('\nThe remaining functions should all return errors.\n')
+
+# Listings
+err_listings = cmc.listings(start=1, limit='10', convert='EUR', convert_id=None, sort='market_cap')
+test_results(err_listings, 'Error Listings')
+
+# Quotes
+err_quotes = cmc.quotes(coinId=None, slug=None)
+test_results(err_quotes, 'Error Quotes')
+
+# Convert Price
+err_convert = cmc.convert_price(1.5e9, coinId=None, symbol='ETH', convert='USD')
+test_results(err_convert, 'Convert')
